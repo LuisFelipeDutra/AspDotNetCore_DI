3368a92fb100c0f68b3feec3af6cf23a148e8236
Luis_Dutra <Luis_Dutra@Dell.com>
2020-12-03 12:03:18 -0300

    Added Interface and it's implementation

---
+++ AspDotNetCore_DI/Models/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AspDotNetCore_DI.Models
+{
+    public interface IUserRepository
+    {
+        List<User> GetAllUsers();
+    }
+}
---
+++ AspDotNetCore_DI/Models/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNetCore_DI.Models
+{
+    public class UserRepository : IUserRepository
+    {
+        public List<User> GetAllUsers()
+        {
+            return new List<User>
+            {
+                new User()
+                {
+                    Name = "Diego",
+                    LastName = "Maradona",
+                    Birth = new DateTime(1960, 10, 30),
+                    UserName = "El Pelusa"
+                },
+                new User()
+                {
+                    Name = "Luis",
+                    LastName = "Dutra",
+                    Birth = new DateTime(1992, 3, 28),
+                    UserName = "Dutra"
+                }
+            };
+        }
+    }
+}
