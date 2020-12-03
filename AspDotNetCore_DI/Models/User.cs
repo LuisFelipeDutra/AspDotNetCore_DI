6eca9cdcb527b4871d739da2da8fc057a5655a79
Luis_Dutra <Luis_Dutra@Dell.com>
2020-12-03 11:51:45 -0300

    Folder and Class creation

---
+++ AspDotNetCore_DI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspDotNetCore_DI.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birth { get; set; }
+        public string UserName { get; set; }
+    }
+}
