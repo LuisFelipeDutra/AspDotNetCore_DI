83e1407d6c4774fb4ecc16b3317bfa51d9ccf1a3
Luis_Dutra <Luis_Dutra@Dell.com>
2020-12-03 12:17:53 -0300

    Created Controller and updated startup

---
+++ AspDotNetCore_DI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using AspDotNetCore_DI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCore_DI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserRepository _userRepo;
+
+        public HomeController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_userRepo.GetAllUsers());
+        }
+    }
+}
--- AspDotNetCore_DI/Startup.cs
+++ AspDotNetCore_DI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspDotNetCore_DI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,8 @@ namespace AspDotNetCore_DI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddTransient<IUserRepository, UserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
